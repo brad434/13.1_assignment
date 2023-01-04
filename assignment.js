96b44b497f9da6a776a39fbed8471febf2cb9e49
Bradley Chery <Bradley434@hotmail.com>
2023-01-04 00:28:38 -0500

    regular work

--- assignment.js
+++ assignment.js
@@ -60,3 +60,87 @@ function vowelCount(str){
     });    
     return newObj;
 };
+
+//MAP
+//doubleValuesWithMap
+
+function doubleValuesWithMap(arr){
+    return arr.map(function(val){
+        return val * 2;
+    });
+}
+
+//valTimesIndex
+//every single value is multipled by its index to give you an answer
+function valTimesIndex(arr){
+    return arr.map(function(val, i){
+        return val * i;
+    })
+}
+
+//extractKey
+function extractKey(arr, key){
+    return arr.map(function(val){
+        return val[key];
+    });
+}
+
+//extractFullName 
+function extractFullName(arr){
+    return arr.map(function(element){
+        return element.first + " " + element.last;
+    });
+}
+
+//--------------- FILTER METHOD ---------------------
+
+//filterByValue
+function filterByValue(arr, key){
+    return arr.filter(function(element){
+        return element[key] !== undefined;
+    });
+}
+
+//find
+function find(arr, value){
+    return arr.filter(function(val){
+        return val === value;
+    })
+    [0];
+}
+
+//findInObj
+function findInObj(arr,key,searchValue){
+    return arr.filter(function(val){
+        return val[key] === searchValue;
+    })[0];
+}
+
+//removeVowels
+/*
+function removeVow(arr){
+    return arr.filter(function(val){
+        const smallVal = val.toLowerCase();
+        if (smallVal !== 'a' || 'e' || 'i' || 'o' || 'u'){
+            return smallVal;
+        };
+    });
+}
+*/
+
+//removeVowels
+function removeVowelss(str){
+    const vowelss = "aeiou";
+    return str.toLowerCase().split("").filter(function(val){
+        return vowelss.indexOf(val) === -1;
+    }).join("");
+}
+
+//DoubleOddNumbers
+function doubleOddNumbers(arr){
+    return arr.filter(function(val){
+        return val % 2 === 1
+    }).map(function(values){
+        return values * 2;
+    })
+}
